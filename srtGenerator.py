5ed45b538d7901f0b57304b0d17e3f1ccedf8b43
AnuragDubey <124233582+AnuragDubeyGame@users.noreply.github.com>
2023-08-19 17:44:09 +0530

    Create srtGenerator.py

---
+++ srtGenerator.py
@@ -0,0 +1,40 @@
+import subprocess
+import assemblyai as aai
+import os
+
+aai.settings.api_key = "291a8c32eb0c4807ad04a757d09425a6"
+
+transcriber = aai.Transcriber()
+transcript = transcriber.transcribe("fact.mp3")
+srt_content = transcript.export_subtitles_srt()
+
+# Save SRT content to a file
+srt_file_path = "subtitlesText.srt"
+with open(srt_file_path, "w") as f:
+    f.write(srt_content)
+
+print(f"SRT subtitles saved to {srt_file_path}")
+
+
+def overlay_subtitles(input_video_path, output_video_path, subtitles_file_path):
+    # FFMPEG command to overlay subtitles on the video
+    ffmpeg_cmd = [
+        "ffmpeg",
+        "-i", input_video_path,
+        "-vf", f"subtitles={subtitles_file_path}",  # Overlay subtitles
+        "-c:a", "copy",  # Copy audio stream
+        output_video_path
+    ]
+    
+    # Run the FFMPEG command
+    subprocess.run(ffmpeg_cmd, shell=True)
+
+if __name__ == "__main__":
+    input_video_path = "MainVideo.mp4"
+    output_video_path = "output_video_with_subtitles.mp4"
+    subtitles_file_path = "subtitlesText.srt"
+    
+    overlay_subtitles(input_video_path, output_video_path, subtitles_file_path)
+
+    # Remove the temporary subtitle file on Windows
+    # os.remove(subtitles_file_path)
